7bd38e546d009d5fd5dc3b8d4bedf661a211e3f6
ixora <fatimacivan@yahoo.com>
2022-05-05 23:52:27 +0300

    sayac
    proje bitimi

---
+++ sayac.js
@@ -0,0 +1,6 @@
+let user = prompt ('Lütfen isminizi giriniz.')
+let date = new Date();
+let kisi = document.querySelector("#myName");
+myName.innerHTML = `${user}`;
+let saat = document.querySelector("#myClock");
+myClock.innerHTML =`${date}`
